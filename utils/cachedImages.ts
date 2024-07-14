5c995b8c71e206462c8eeb9fa232ac23d1d647ed
Sagaust <austinorge@yahoo.com>
2024-07-14 00:11:27 +0000

    cachedImages.ts issues

--- utils/cachedImages.ts
+++ utils/cachedImages.ts
@@ -1,10 +1,10 @@
 import cloudinary from "./cloudinary";
 
-let cachedResults;
+let cachedResults: any; // Or provide a more specific type if known
 
 export default async function getResults() {
   if (!cachedResults) {
-    const fetchedResults = await cloudinary.v2.search
+    const fetchedResults = await cloudinary.search // Removed 'v2' here
       .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
       .sort_by("public_id", "desc")
       .max_results(400)
