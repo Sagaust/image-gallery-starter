d149f75d032e1b9e27470363ae6877a55ab3aad1
Sagaust <austinorge@yahoo.com>
2024-07-14 00:26:40 +0000

    still on it

--- utils/cloudinary.ts
+++ utils/cloudinary.ts
@@ -1,8 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 import dotenv from 'dotenv';
+import path from 'path'; // Import the path module
 
 // Load environment variables from .env.local file
-dotenv.config({ path: path.join(__dirname, '../.env.local') }); 
+dotenv.config({ path: path.join(__dirname, '../.env.local') });
 
 cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
