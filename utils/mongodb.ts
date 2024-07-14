876e4c824805a203cf5fa759bae6822178356a6c
Sagaust <austinorge@yahoo.com>
2024-07-14 00:16:43 +0000

    still on it

--- utils/mongodb.ts
+++ utils/mongodb.ts
@@ -1,11 +1,22 @@
 import { MongoClient } from 'mongodb';
 
-const client = new MongoClient(process.env.MONGODB_URI); // Remove options
+const client = new MongoClient(process.env.MONGODB_URI);
 
 async function connectToDatabase() {
-  if (!client.isConnected()) await client.connect();
-  const db = client.db(process.env.MONGODB_DB);
-  return { db, client };
+  try {
+    // Check if client is already connected
+    if (!client.topology || !client.topology.isConnected()) { 
+      // If not, try to establish a connection
+      await client.connect(); 
+    }
+    
+    const db = client.db(process.env.MONGODB_DB);
+    return { db, client };
+  } catch (err) {
+    // Handle connection error
+    console.error('Failed to connect to MongoDB:', err);
+    throw err; // Rethrow the error or handle it gracefully
+  }
 }
 
 export default connectToDatabase;
