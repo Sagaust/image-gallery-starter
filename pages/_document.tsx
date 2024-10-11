050b7887977ca7484ee709ce0ea9861fed055d8c
Sagaust <austinorge@yahoo.com>
2024-10-11 03:48:51 -0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           <meta
-            name="Philos-DH"
+            name="Philosophy AI LAB"
             content="Learning Philosophy through Visualization"
           />
           
